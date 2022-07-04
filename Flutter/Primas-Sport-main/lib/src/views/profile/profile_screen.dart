1fd28ca3163907341b0dafff10f4fee6254b76d4
SatThu <minhduc.nt1602@gmail.com>
2022-07-04 23:17:14 +0700

    Update UI & UX

---
+++ Flutter/Primas-Sport-main/lib/src/views/profile/profile_screen.dart
@@ -0,0 +1,13 @@
+part of lib_profile;
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar2,
+      body: const Body(),
+    );
+  }
+}
